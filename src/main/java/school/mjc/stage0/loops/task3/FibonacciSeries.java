dd93b368da006c3812ec4147ad97bb15fc946117
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-01-27 16:33:22 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int[] arr = new int[lastFibonacci];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; ++i) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.println(arr[i]);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -3,6 +3,16 @@ package school.mjc.stage0.loops.task3;
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
 
+        int a = 9;
+        int b = 11;
+        for (int i = 0; i < lengthOfLastNumber; i++){
+            a += 9 * b;
+            b *= 10 + 1;
+
+
+        }
+        System.out.println(a);
+
 
     }
 }
