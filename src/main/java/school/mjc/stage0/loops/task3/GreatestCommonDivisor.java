2f80795a636d82a85f147e206c893c5aef22fa55
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-02-01 22:30:20 +0300

    doine

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -1,18 +1,14 @@
 package school.mjc.stage0.loops.task3;
 
+import java.math.BigInteger;
+
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
         int a = first;
         int b = second;
-        while (a != b){
-            if (a > b){
-                a -= b;
-            }
-            else {
-                b -= a;
-            }
-
-        }
-        System.out.println(a);
+        BigInteger b1 = BigInteger.valueOf(a);
+        BigInteger b2 = BigInteger.valueOf(b);
+        BigInteger gcd = b1.gcd(b2);
+        System.out.println(gcd.intValue());
     }
 }
